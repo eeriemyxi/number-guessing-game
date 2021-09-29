77ad7bdaff68c65a58111ab41c40f45527bef8c3
I-am-Myxi <mail@myxi.tk>
2021-09-29 13:24:49 +0530

    Now it's kinda playable

--- command_functions.py
+++ command_functions.py
@@ -9,11 +9,24 @@ DIFFICULTIES = {
 }
 fancytext = pyfiglet.figlet_format
 
-# def point_deduct(var, difficulty):
-#     return (var + difficulty.get('points')) - difficulty.get('deduct') if var == 0 else var - difficulty.get('deduct')
+
+def get_hint(number, difficulty):
+    max_points = difficulty.get("points")
+    random_number = random.randint(2, difficulty.get("points"))
+    if number >= random_number:
+        return "The number you have to guess is greater than or equal to {} and less than {}".format(
+            random_number, max_points
+        )
+    elif number <= random_number:
+        return "The number you have to guess is less than or equal to {}".format(
+            random_number
+        )
+
 
 def point_deduct(var, difficulty):
-    return var - difficulty.get('deduct')
+    return var - difficulty.get("deduct")
+
+
 def verify_difficulty(difficulty_name):
     if difficulty_name not in DIFFICULTIES:
         print(fancytext("Invalid difficulty"))
@@ -43,54 +56,74 @@ class CommandFunctions:
 
         selected_difficulty = DIFFICULTIES[selected_difficulty]
         game_round = 0
-        guess_number = random.randint(*selected_difficulty.get('range'))
-        status = 200
+        wrong_guesses = 0
+        guess_number = random.randint(*selected_difficulty.get("range"))
         chances = 3
+        hints = 3
         won = 0
         protection = True
         points = 0
-        while game_round >= 10 or status != 500:
-            round_points = 0
+        round_points = 0
+        while True:
+            if game_round >= 10:
+                print(fancytext("Game complete."))
+                print(
+                    fancytext(
+                        "Total points:\n(  {won} x {max_points_per_round}  ) - (  {deduct} x {wrong_guesses}  )\n= {sum}".format(
+                            won=won,
+                            max_points_per_round=selected_difficulty.get("points"),
+                            deduct=selected_difficulty.get("deduct"),
+                            wrong_guesses=wrong_guesses,
+                            sum=points,
+                        )
+                    )
+                )
+                break
             if chances <= 0:
                 game_round += 1
                 chances = 3
                 points = points + round_points
-
-                print(
-                    fancytext(f'Round complete! Total points: {points}')
-                )
-            game_input = input(f'[{chances} chances left] Guess: ')
-            try: game_input = int(game_input)
+                round_points = 0
+                print(fancytext(f"Round complete! Total points: {points}"))
+            game_input = input(f"[{chances} chances and {hints} hints left] Guess: ")
+            try:
+                game_input = int(game_input)
             except ValueError:
-                if game_input in ('cancel', 'hint'):
-                    if game_input == 'cancel':
+                if game_input in ("cancel", "hint"):
+                    if game_input == "cancel":
                         break
                     else:
-                        print('HINT FEATURE IS COMING SOON')
+                        if hints > 0:
+                            print(get_hint(guess_number, selected_difficulty))
+                            hints -= 1
+                        else:
+                            print("You have already wasted all your hints.")
                 else:
                     if protection:
-                        print("Why are you trying to waste your chances? This is your last warning.")
+                        print(
+                            "Why are you trying to waste your chances? This is your last warning."
+                        )
                         protection = False
                     else:
-                        print('Wrong guess.')
+                        print("Wrong guess.")
                         chances -= 1
+                        wrong_guesses += 1
                         round_points = point_deduct(round_points, selected_difficulty)
-
-            if game_input == guess_number:
-                print(
-                    'Correct guess!'
-                )
-                points = points + round_points + selected_difficulty['points']
-                game_round += 1
-                chances = 3
-                print(
-                    fancytext(f'Round complete! Total points: {points}')
-                )
             else:
-                print('Wrong guess.')
-                chances -= 1
-                points = points + point_deduct(round_points, selected_difficulty)
-                print(points)
+                if game_input == guess_number:
+                    print("Correct guess!")
+                    points = points + round_points + selected_difficulty["points"]
+                    game_round += 1
+                    chances = 3
+                    round_points = 0
+                    won += 1
+                    print(fancytext(f"Round complete! Total points: {points}"))
+                else:
+                    print("Wrong guess.")
+                    chances -= 1
+                    wrong_guesses += 1
+                    points += point_deduct(round_points, selected_difficulty)
+
     def info(self, input_text):
         print("Called the info funtion!")
 
